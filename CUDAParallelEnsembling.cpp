9e947e098be38d0c888b0856327ac0d7f6bcd831
Tokarev Artem Alekseevich <tokarev_28@mail.ru>
2018-07-17 23:28:29 +0500

    Concretized

--- CUDAParallelEnsembling.cpp
+++ CUDAParallelEnsembling.cpp
@@ -12,15 +12,15 @@
 // Размер: DIM*nodesNum x DIM*nodesNum
 //
 // Np[i][j] - глобальный номер узла.
-//    i     - глобальный номер элемента,
+//    i     - глобальный номер конечного элемента,
 //       j  - локальный  номер узла.
 // Размер: elemsNum x Npt
 //
-// K[i][j][k] - значение из локальной матрицы.
-//   i        - глобальный номер элемента,
+// K[i][j][k] - значение из локальной матрицы жёсткости.
+//   i        - глобальный номер конечного элемента,
 //      j     - строка,
 //         k  - столбец.
-// Размер: elemsNum x 2*DIM*Npt x 2*DIM*Npt
+// Размер: elemsNum x DIM*Npt x DIM*Npt
 //
 // elemsNum - общее кол-во элементов.
 //
@@ -34,14 +34,14 @@
 // maxNconc = max(Nconc, nodesNum);
 // где max - функция, вычисляющая наибольшее значение массива.
 //
-// Elem_Conc[i][j] - глобальный номер элемента.
+// Elem_Conc[i][j] - глобальный номер конечного элемента.
 //           i     - глобальный номер узла,
-//              j  - локальный номер элемента.
+//              j  - локальный номер конечного элемента.
 // Размер: nodesNum x maxNconc
 //
 // No_Corresp[i][j] - локальный номер узла.
 //            i     - глобальный номер узла,
-//               j  - локальный номер элемента.
+//               j  - локальный номер конечного элемента.
 // Размер: nodesNum x maxNconc
 
 // Считается, что входные матрицы представляют собой каждая единый неразрывный блок памяти.
@@ -49,10 +49,10 @@
 
 template<class T>
 __global__
-void ParallelEnsemble(T* d_glM, size_t* d_Np, T* d_K, size_t* d_Nconc, size_t* d_Elem_Conc, size_t* d_No_Corresp, size_t* d_nodesNum, size_t* d_maxNconc)
+void ParallelEnsemble(T* glM, size_t* Np, T* K, size_t* Nconc, size_t* Elem_Conc, size_t* No_Corresp, size_t* nodesNum, size_t* maxNconc)
 {
-	size_t nodesNum = *d_nodesNum;
-	size_t maxNconc = *d_maxNconc;
+	size_t nodesNum = *nodesNum;
+	size_t maxNconc = *maxNconc;
 
 	size_t node_loc_num;
 	size_t elem_glob_num;
@@ -64,29 +64,28 @@ void ParallelEnsemble(T* d_glM, size_t* d_Np, T* d_K, size_t* d_Nconc, size_t* d
 	size_t index = blockIdx.x * blockDim.x + threadIdx.x;
 	if (index < nodesNum)
 	{
-		size_t twoDIMNpt = 2 * DIM * Npt;
-		for (size_t j = 0; j < d_Nconc[index]; j++)
+		for (size_t j = 0; j < Nconc[index]; j++)
 		{
-			node_loc_num  = d_No_Corresp[index * maxNconc + j];
-			elem_glob_num = d_Elem_Conc [index * maxNconc + j];
+			node_loc_num  = No_Corresp[index * maxNconc + j];
+			elem_glob_num = Elem_Conc [index * maxNconc + j];
 
-			ii = DIM * d_Np[elem_glob_num * Npt + node_loc_num];
+			ii = DIM * Np[elem_glob_num * Npt + node_loc_num];
 			for (int k = 0; k < Npt; k++)
 			{
-				block[0] = &d_K[elem_glob_num * twoDIMNpt * twoDIMNpt +  DIM * node_loc_num      * twoDIMNpt + DIM * k];
-				block[1] = &d_K[elem_glob_num * twoDIMNpt * twoDIMNpt + (DIM * node_loc_num + 1) * twoDIMNpt + DIM * k];
-				block[2] = &d_K[elem_glob_num * twoDIMNpt * twoDIMNpt + (DIM * node_loc_num + 2) * twoDIMNpt + DIM * k];
+				block[0] = &K[elem_glob_num * DIM * DIM * Npt * Npt +  DIM * node_loc_num      * DIM * Npt + DIM * k];
+				block[1] = &K[elem_glob_num * DIM * DIM * Npt * Npt + (DIM * node_loc_num + 1) * DIM * Npt + DIM * k];
+				block[2] = &K[elem_glob_num * DIM * DIM * Npt * Npt + (DIM * node_loc_num + 2) * DIM * Npt + DIM * k];
 
-				sharedBlock[      0] = block[0][0]; sharedBlock[          1] = block[0][1]; sharedBlock[          2] = block[0][2];
-				sharedBlock[    DIM] = block[1][0]; sharedBlock[    DIM + 1] = block[1][1]; sharedBlock[    DIM + 2] = block[1][2];
-				sharedBlock[2 * DIM] = block[2][0]; sharedBlock[2 * DIM + 1] = block[2][1]; sharedBlock[2 * DIM + 2] = block[2][2];
+				sharedBlock[          0] = block[0][0]; sharedBlock[          1] = block[0][1]; sharedBlock[          2] = block[0][2];
+				sharedBlock[    DIM    ] = block[1][0]; sharedBlock[    DIM + 1] = block[1][1]; sharedBlock[    DIM + 2] = block[1][2];
+				sharedBlock[2 * DIM    ] = block[2][0]; sharedBlock[2 * DIM + 1] = block[2][1]; sharedBlock[2 * DIM + 2] = block[2][2];
 
 				// Проверка не заполнен ли блок нулями.
 				if (!IsZero3x3<T>(sharedBlock))
 				{
-					jj = DIM * d_Np[elem_glob_num * Npt + k];
+					jj = DIM * Np[elem_glob_num * Npt + k];
 
-					Add3x3<T>(d_glM, nodesNum, ii, jj, sharedBlock);
+					Add3x3<T>(glM, nodesNum, ii, jj, sharedBlock);
 				}
 			}
 		}
@@ -99,7 +98,7 @@ bool IsZero3x3(T* block)
 {
 	for (int i = 0; i < 3; i++)
 	{
-		if ((block[i * DIM] < -1e-5 || block[i * DIM] > 1e-5))
+		if ((block[i * DIM    ] < -1e-5 || block[i * DIM    ] > 1e-5))
 		{
 			return false;
 		}
@@ -118,14 +117,14 @@ bool IsZero3x3(T* block)
 
 template<class T>
 __device__
-void Add3x3(T* &d_glM, size_t &nodesNum, size_t &ii, size_t &jj, T* block)
+void Add3x3(T* &glM, size_t &nodesNum, size_t &ii, size_t &jj, T* block)
 {
 	size_t index = ii * DIM * nodesNum + jj;
-	d_glM[index] += block[      0]; d_glM[index + 1] += block[          1]; d_glM[index + 2] += block[          2];
+	glM[index] += block[      0]; glM[index + 1] += block[          1]; glM[index + 2] += block[          2];
 
 	index += DIM * nodesNum;
-	d_glM[index] += block[    DIM]; d_glM[index + 1] += block[    DIM + 1]; d_glM[index + 2] += block[    DIM + 2];
+	glM[index] += block[    DIM]; glM[index + 1] += block[    DIM + 1]; glM[index + 2] += block[    DIM + 2];
 
 	index += DIM * nodesNum;
-	d_glM[index] += block[2 * DIM]; d_glM[index + 1] += block[2 * DIM + 1]; d_glM[index + 2] += block[2 * DIM + 2];
+	glM[index] += block[2 * DIM]; glM[index + 1] += block[2 * DIM + 1]; glM[index + 2] += block[2 * DIM + 2];
 }
